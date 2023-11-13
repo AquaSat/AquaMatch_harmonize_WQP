0fcaebf60ee48fc1eb9fe8e01ac9cc7ae19b380f
B Steele <B.Steele@colostate.edu>
2023-11-13 11:48:53 -0700

    add package list and package installer to run.r

--- run.R
+++ run.R
@@ -1,5 +1,31 @@
 #!/usr/bin/env Rscript
 
+# list of packages required for this pipeline
+required_pkgs <- c("targets", 
+  "tarchetypes",
+  "sf",
+  "tidyverse",
+  "feather",
+  "lubridate",
+  "ggrepel",
+  "scales",
+  "tigris",
+  "tictoc")
+
+# helper function to install all necessary pacakges
+package_installer <- function(x) {
+  if (x %in% installed.packages()) {
+    print(paste0("{", x ,"} package is already installed."))
+  } else {
+    install.packages(x)
+    print(paste0("{", x ,"} package has been installed."))
+  }
+}
+
+# map function using base lapply
+lapply(packages, package_installer)
+
+
 library(targets)
 
 # This is a helper script to run the pipeline.

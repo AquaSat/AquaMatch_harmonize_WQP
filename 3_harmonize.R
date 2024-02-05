e761bfa05a13bc7acb4964d540d4a079f9f5687a
Matt Brousil <37380883+mbrousil@users.noreply.github.com>
2024-02-05 11:19:30 -0800

    Formatting on targets lists

--- 3_harmonize.R
+++ 3_harmonize.R
@@ -18,47 +18,53 @@ p3_targets_list <- list(
   # undesired variables can also be dropped from the WQP dataset using the 
   # optional `drop_vars` argument. 
   tar_target(
-    p3_wqp_data_aoi_formatted_chl,
-    format_columns(p2_wqp_data_aoi_chl),
+    name = p3_wqp_data_aoi_formatted_chl,
+    command = format_columns(p2_wqp_data_aoi_chl),
     format = "feather"
   ),
   
   tar_target(
-    p3_wqp_data_aoi_formatted_doc,
-    format_columns(p2_wqp_data_aoi_doc),
+    name = p3_wqp_data_aoi_formatted_doc,
+    command = format_columns(p2_wqp_data_aoi_doc),
     format = "feather"
   ),
   
   # Pre-harmonization: chlorophyll ------------------------------------------
   tar_target(
-    p3_wqp_data_aoi_ready_chl,
-    clean_wqp_data(wqp_data = p3_wqp_data_aoi_formatted_chl,
-                   char_names_crosswalk = p1_char_names_crosswalk,
-                   # Convert list of sites by param to single df
-                   site_data = bind_rows(p2_site_counts),
-                   wqp_metadata = p1_wqp_inventory_aoi),
-    packages = c("tidyverse", "feather")),
+    name = p3_wqp_data_aoi_ready_chl,
+    command = clean_wqp_data(wqp_data = p3_wqp_data_aoi_formatted_chl,
+                             char_names_crosswalk = p1_char_names_crosswalk,
+                             # Convert list of sites by param to single df
+                             site_data = bind_rows(p2_site_counts),
+                             wqp_metadata = p1_wqp_inventory_aoi),
+    packages = c("tidyverse", "feather")
+  ),
   
   # Connect cleaned data output to the pipeline
-  tar_target(p3_cleaned_wqp_data_chl,
-             read_feather(p3_wqp_data_aoi_ready_chl$wqp_data_clean_path),
-             packages = "feather",
-             format = "feather"),
+  tar_target(
+    name = p3_cleaned_wqp_data_chl,
+    command = read_feather(p3_wqp_data_aoi_ready_chl$wqp_data_clean_path),
+    packages = "feather",
+    format = "feather"
+  ),
   
   # Pre-harmonization: DOC --------------------------------------------------
   tar_target(
-    p3_wqp_data_aoi_ready_doc,
-    clean_wqp_data(wqp_data = p3_wqp_data_aoi_formatted_doc,
-                   char_names_crosswalk = p1_char_names_crosswalk,
-                   # Convert list of sites by param to single df
-                   site_data = bind_rows(p2_site_counts),
-                   wqp_metadata = p1_wqp_inventory_aoi),
-    packages = c("tidyverse", "feather")),
+    name = p3_wqp_data_aoi_ready_doc,
+    command = clean_wqp_data(wqp_data = p3_wqp_data_aoi_formatted_doc,
+                             char_names_crosswalk = p1_char_names_crosswalk,
+                             # Convert list of sites by param to single df
+                             site_data = bind_rows(p2_site_counts),
+                             wqp_metadata = p1_wqp_inventory_aoi),
+    packages = c("tidyverse", "feather")
+  ),
   
-  tar_target(p3_cleaned_wqp_data_doc,
-             read_feather(p3_wqp_data_aoi_ready_doc$wqp_data_clean_path),
-             packages = "feather",
-             format = "feather"),
+  tar_target(
+    name = p3_cleaned_wqp_data_doc,
+    command = read_feather(p3_wqp_data_aoi_ready_doc$wqp_data_clean_path),
+    packages = "feather",
+    format = "feather"
+  ),
   
   
   # Harmonization process ---------------------------------------------------
@@ -70,59 +76,75 @@ p3_targets_list <- list(
     packages = c("tidyverse", "rvest", "janitor")
   ),
   
-  # Harmonization: chlorophyll ----------------------------------------------
-  tar_target(p3_chla_harmonized,
-             harmonize_chla(raw_chla = p3_cleaned_wqp_data_chl,
-                            p_codes = p3_p_codes),
-             packages = c("tidyverse", "feather", "ggrepel", "scales")),
   
-  tar_file_read(name = p3_chla_tiering_record,
-                command = p3_chla_harmonized$chla_tiering_record_path,
-                read = read_csv(file = !!.x)),
+  # Harmonization: chlorophyll ----------------------------------------------
+  tar_target(
+    name = p3_chla_harmonized,
+    command = harmonize_chla(raw_chla = p3_cleaned_wqp_data_chl,
+                             p_codes = p3_p_codes),
+    packages = c("tidyverse", "feather", "ggrepel", "scales")
+  ),
+  
+  tar_file_read(
+    name = p3_chla_tiering_record,
+    command = p3_chla_harmonized$chla_tiering_record_path,
+    read = read_csv(file = !!.x)
+  ),
   
   # Harmonized chlorophyll data containing grouping IDs for simultaneous
   # records, but not aggregated
-  tar_file_read(name = p3_chla_preagg_grouped,
-                command = p3_chla_harmonized$chla_grouped_preagg_path,
-                read = read_feather(path = !!.x),
-                cue = tar_cue("always"),
-                packages = "feather"),
+  tar_file_read(
+    name = p3_chla_preagg_grouped,
+    command = p3_chla_harmonized$chla_grouped_preagg_path,
+    read = read_feather(path = !!.x),
+    cue = tar_cue("always"),
+    packages = "feather"),
   
   # Harmonized chlorophyll data after simultaneous record aggregation (i.e.,
   # final product)
-  tar_file_read(name = p3_chla_agg_harmonized,
-                command = p3_chla_harmonized$chla_harmonized_path,
-                read = read_csv(file = !!.x)),
+  tar_file_read(
+    name = p3_chla_agg_harmonized,
+    command = p3_chla_harmonized$chla_harmonized_path,
+    read = read_csv(file = !!.x)),
   
   
   # Harmonization: DOC ------------------------------------------------------
   
-  tar_target(p3_doc_harmonized,
-             harmonize_doc(raw_doc = p3_cleaned_wqp_data_doc,
-                           p_codes = p3_p_codes),
-             packages = c("tidyverse", "feather", "ggrepel", "scales")),
+  tar_target(
+    name = p3_doc_harmonized,
+    command = harmonize_doc(raw_doc = p3_cleaned_wqp_data_doc,
+                            p_codes = p3_p_codes),
+    packages = c("tidyverse", "feather", "ggrepel", "scales")
+  ),
   
-  tar_file_read(name = p3_doc_tiering_record,
-                command = p3_doc_harmonized$doc_tiering_record_path,
-                read = read_csv(file = !!.x)),
+  tar_file_read(
+    name = p3_doc_tiering_record,
+    command = p3_doc_harmonized$doc_tiering_record_path,
+    read = read_csv(file = !!.x)
+  ),
   
-  tar_file_read(name = p3_doc_preagg_grouped,
-                command = p3_doc_harmonized$doc_grouped_preagg_path,
-                read = read_feather(path = !!.x),
-                packages = "feather"),
+  tar_file_read(
+    name = p3_doc_preagg_grouped,
+    command = p3_doc_harmonized$doc_grouped_preagg_path,
+    read = read_feather(path = !!.x),
+    packages = "feather"),
   
-  tar_file_read(name = p3_doc_agg_harmonized,
-                command = p3_doc_harmonized$doc_harmonized_path,
-                read = read_csv(file = !!.x)),
+  tar_file_read(
+    name = p3_doc_agg_harmonized,
+    command = p3_doc_harmonized$doc_harmonized_path,
+    read = read_csv(file = !!.x)
+  ),
   
   
   # Documentation of dropped records ----------------------------------------
   
-  tar_target(p3_documented_drops,
-             map_df(.x = c(p3_wqp_data_aoi_ready_chl$compiled_drops_path,
-                           p3_wqp_data_aoi_ready_doc$compiled_drops_path,
-                           p3_chla_harmonized$compiled_drops_path,
-                           p3_doc_harmonized$compiled_drops_path),
-                    .f = read_csv))
+  tar_target(
+    name = p3_documented_drops,
+    command = map_df(.x = c(p3_wqp_data_aoi_ready_chl$compiled_drops_path,
+                            p3_wqp_data_aoi_ready_doc$compiled_drops_path,
+                            p3_chla_harmonized$compiled_drops_path,
+                            p3_doc_harmonized$compiled_drops_path),
+                     .f = read_csv)
+  )
 )
 
